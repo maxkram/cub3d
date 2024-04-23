e79c94f5e48fcc55a4ca40d1a1d28205a61c0246
maxkram <max.kramer@live.com>
2024-04-23 01:01:09 -0400

    first commit

---
+++ cub3d.h
@@ -0,0 +1,11 @@
+#ifndef CUB3D_H
+#define CUB3D_H
+
+# include <math.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "parsing.h"
+# include "./MLX42.h"
+
+#endif
---
+++ parsing.h
@@ -0,0 +1,6 @@
+#ifndef PARSING_H
+# define PARSING_H
+
+# include "cub3d.h"
+
+#endif
